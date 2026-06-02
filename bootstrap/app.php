d84fb11128b0deed1e6939240f8b586345e0bd41
Alaa m ebrahim <alaamebrahim91@gmail.com>
2026-06-02 20:22:19 +0300

    force https on production

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,10 +10,7 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        if (config('app.env') === 'production') {
-            $middleware->trustProxies(at: '*');
-        }
+    ->withMiddleware(function (Middleware $middleware): void { 
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
